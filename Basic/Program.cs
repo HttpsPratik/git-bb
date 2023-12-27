b953e6e11ac975d47b397e67d13b4801d6376260
HtppsPratik <pratik.thapa.1223@gmail.com>
2023-12-27 13:49:06 +0545

    anything

---
+++ Basic/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Basic
+{
+    internal class Program { 
+class Car
+{
+    string color = "red";
+    int maxspeed = 200;
+
+    static void Main(string[]args ){
+    Car myObj = new Car();
+
+    Console.WriteLine(myObj.color);
+    Console.WriteLine(myObj.maxspeed);
+    }
+}
+    }
+}
